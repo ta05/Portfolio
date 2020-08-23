42a358c9bb55c5a408a8cb78b45c1ba0a9c3d778
Tolu Alimi <tolualimi05@gmail.com>
2020-08-23 14:06:44 -0500

    Added a style sheet and the images for my portfolio

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -123,4 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
