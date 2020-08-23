39c787745eb150f257b2f800dbe2e55497ae31b7
Tolu Alimi <tolualimi05@gmail.com>
2020-08-23 09:47:04 -0500

    Used .env for secret key instead of get_random_key

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -22,7 +24,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_random_secret_key()
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+# SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
