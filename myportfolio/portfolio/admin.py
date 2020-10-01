b1366c95ec271469fbc19eb3ba11696a840bc63f
Tolu Alimi <tolualimi05@gmail.com>
2020-10-01 10:56:10 -0500

    Fixed inside job images. Incorporated ability to order projects through the admin page.

--- myportfolio/portfolio/admin.py
+++ myportfolio/portfolio/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
+from ordered_model.admin import OrderedModelAdmin
 
 # Register your models here.
 from .models import Project
+
+class ProjectAdmin(OrderedModelAdmin):
+    list_display = ('title', 'move_up_down_links')
 
 admin.site.site_header = "Portfolio Admin"
 admin.site.site_title = "Projects"
 admin.site.index_title = "Welcome to the Projects Admin Area"
 
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
--- myportfolio/portfolio/models.py
+++ myportfolio/portfolio/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from ordered_model.models import OrderedModel
 
 # Create your models here.
-class Project(models.Model):
+class Project(OrderedModel):
     title = models.CharField(max_length=255)
     description = models.TextField()
     imagePath = models.CharField(max_length=255)
@@ -9,6 +10,9 @@ class Project(models.Model):
     fullImagePath = models.CharField(max_length=255)
     siteURL = models.CharField(max_length=255)
     gitURL = models.CharField(max_length=255)
+
+    class Meta(OrderedModel.Meta):
+        pass
 
     def __str__(self):
         return self.title
