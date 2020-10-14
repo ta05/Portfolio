c5ac40baf475af3feb8c3b4b701843bf87328ab9
Tolu Alimi <tolualimi05@gmail.com>
2020-10-14 18:46:49 -0500

    Completed Contact page formatting

--- myportfolio/sendemail/forms.py
+++ myportfolio/sendemail/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class ContactForm(forms.Form):
     email = forms.EmailField(required=True)
     subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    message = forms.CharField(widget=forms.Textarea(attrs={'rows': 10, 'style': "width: 100%"}), required=True)
