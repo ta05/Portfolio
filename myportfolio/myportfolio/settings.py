ecb0aaa6307343b802eaa73d5da4ee1b2af59a3b
Tolu Alimi <tolualimi05@gmail.com>
2021-11-07 15:40:46 -0600

    turned off debug mode

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
